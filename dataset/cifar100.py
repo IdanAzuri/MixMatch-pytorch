02d18d4710fba06a87cedb8e4ec605f2cd2eae3b
idanazuri <idan.azuri@gmail.com>
2019-12-27 01:22:22 +0200

    adding cifar100

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -17,7 +17,7 @@ def get_cifar100(root, n_labeled,
                  transform_train=None, transform_val=None,
                  download=True):
 
-    base_dataset = torchvision.datasets.CIFAR0(root, train=True, download=download)
+    base_dataset = torchvision.datasets.CIFAR100(root, train=True, download=download)
     train_labeled_idxs, train_unlabeled_idxs, val_idxs = train_val_split(base_dataset.targets, int(n_labeled/10))
 
     train_labeled_dataset = CIFAR100_labeled(root, train_labeled_idxs, train=True, transform=transform_train)
